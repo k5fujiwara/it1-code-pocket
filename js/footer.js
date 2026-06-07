559e58b2da2ea3dcd390d133143185f764c2ab3f
keigo-fujiwara <keigo_fujiwara@waotech.jp>
2026-06-08 04:19:48 +0900

    LINEボタンの修正

--- js/footer.js
+++ js/footer.js
@@ -14,11 +14,12 @@ function renderSiteFooter() {
   const isTopPage = currentPage === '' || currentPage === 'index.html';
   const shareUrl = encodeURIComponent('https://it1-code-pocket.com/');
   const shareText = encodeURIComponent('IT1-CODE-POCKET | 情報Ⅰ 第3問対策');
+  const lineShareText = encodeURIComponent('IT1-CODE-POCKET | 情報Ⅰ 第3問対策\nhttps://it1-code-pocket.com/');
   const shareLinks = isTopPage
     ? `
       <div class="site-footer-share" aria-label="SNSで共有">
         <a class="site-footer-share-btn share-x" href="https://twitter.com/intent/tweet?text=${shareText}&url=${shareUrl}" target="_blank" rel="noopener noreferrer">Xで共有</a>
-        <a class="site-footer-share-btn share-line" href="https://social-plugins.line.me/lineit/share?url=${shareUrl}" target="_blank" rel="noopener noreferrer">LINEで共有</a>
+        <a class="site-footer-share-btn share-line" href="https://line.me/R/msg/text/?${lineShareText}" target="_blank" rel="noopener noreferrer">LINEで共有</a>
         <a class="site-footer-share-btn share-facebook" href="https://www.facebook.com/sharer/sharer.php?u=${shareUrl}" target="_blank" rel="noopener noreferrer">Facebook</a>
       </div>
     `
