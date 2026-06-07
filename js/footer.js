b3a31db16cd5369355952d6ead8e40b6524b8ff1
keigo-fujiwara <keigo_fujiwara@waotech.jp>
2026-06-08 04:27:39 +0900

    SNSボタンの修正

--- js/footer.js
+++ js/footer.js
@@ -15,12 +15,13 @@ function renderSiteFooter() {
   const shareUrl = encodeURIComponent('https://it1-code-pocket.com/');
   const shareText = encodeURIComponent('IT1-CODE-POCKET | 情報Ⅰ 第3問対策');
   const lineShareText = encodeURIComponent('IT1-CODE-POCKET | 情報Ⅰ 第3問対策\nhttps://it1-code-pocket.com/');
+  const facebookQuote = encodeURIComponent('IT1-CODE-POCKET | 情報Ⅰ 第3問対策');
   const shareLinks = isTopPage
     ? `
       <div class="site-footer-share" aria-label="SNSで共有">
         <a class="site-footer-share-btn share-x" href="https://twitter.com/intent/tweet?text=${shareText}&url=${shareUrl}" target="_blank" rel="noopener noreferrer">Xで共有</a>
-        <a class="site-footer-share-btn share-line" href="https://line.me/R/msg/text/?${lineShareText}" target="_blank" rel="noopener noreferrer">LINEで共有</a>
-        <a class="site-footer-share-btn share-facebook" href="https://www.facebook.com/sharer/sharer.php?u=${shareUrl}" target="_blank" rel="noopener noreferrer">Facebook</a>
+        <a class="site-footer-share-btn share-line" href="https://line.me/R/share?text=${lineShareText}">LINEで共有</a>
+        <a class="site-footer-share-btn share-facebook" href="https://www.facebook.com/sharer/sharer.php?u=${shareUrl}&quote=${facebookQuote}" target="_blank" rel="noopener noreferrer">Facebook</a>
       </div>
     `
     : '';
